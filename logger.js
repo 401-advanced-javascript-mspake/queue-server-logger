8928946dd70cd1d39e765bf77cf4e7d0626c6502
Morgana Spake <morgana.spake@gmail.com>
2019-06-16 23:24:55 -0700

    logger

---
+++ logger.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Client = require('@nmq/q/client');
+
+const database = new Client('database');
+
+database.subscribe('create', () => {
+  console.log('created');
+});
+database.subscribe('update', () => {
+  console.log('updated');
+});
+database.subscribe('delete', () => {
+  console.log('deleted');
+});
+database.subscribe('read', () => {
+  console.log('read');
+});
+database.subscribe('error', () => {
+  console.log('error');
+});
